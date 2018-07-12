343b944160dda75b71a0e7a7af53492d82b7bac1
dave <dave@netfm.org>
2018-07-12 15:44:57 +0100

    first commit

---
+++ AddUser.cs
@@ -0,0 +1,232 @@
+using System;
+using System.Data;
+using System.Collections.Generic;
+using Paxton.Net2.OemClientLibrary;
+
+namespace Net2_OEM_SDK
+{
+    class Program
+    {
+        private const int REMOTE_PORT = 8025;
+        private const string REMOTE_HOST = "localhost";
+        private OemClient _net2Client = new OemClient(REMOTE_HOST, REMOTE_PORT);
+        //public int myDepID;
+
+        static void Main(string[] args)
+        {
+            int numID;
+            if (args.Length != 7)
+            {
+                Console.WriteLine("Format:  Sample1.exe [department] [First name] [Second name] [email] [from] [until] [code]");
+                Console.WriteLine("Example: Sample1.exe visitor_05_16 Joe Bloggs joe@gmail.com  2018-05-01 2018-05-01T07:34:42-5:00 1002");
+                return;
+            }
+
+            Program main = new Program();
+
+            bool res = main.AuthenticateUser("System engineer", "isaac0904");
+            if (res != true)
+            {
+                Console.WriteLine("Incorrect username or password");
+                main.Close();
+                return;
+            }
+
+            Console.WriteLine("Authentication success");
+
+
+            IUsers usersSet = main._net2Client.ViewUserRecords(String.Format("Field9_50 = '{0}' AND active=1", args[2]));
+            
+            if (usersSet.UsersList().Count > 0)
+            {
+                Console.WriteLine("xxx Duplicate entry, no thank you!");
+                main.Close();
+                return;
+            }
+
+            //public int Day { get; }
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine(now.Month);
+            Console.WriteLine(now.Day);
+
+            //string depString = "Visitors_"+ now.Month + "_" + now.Day;
+            string depString = args[0];
+
+            Console.WriteLine("department = " + depString);
+
+
+            //private IDepartments xxx;
+
+
+           // foreach (DepartmentsSet.DepartmentRow dept in xxx.DepartmentsDataSource.Department)
+            //{
+           //     Console.WriteLine("department  " + dept.Name + " " + dept.DepartmentID);
+           //     Console.WriteLine("department  " + dept.Name + " " + dept.DepartmentID);
+           // }
+
+                // delete departments XXXwe need to learn how to remove all users from department
+/*
+            //    foreach (DepartmentsSet.DepartmentRow dept in xxx.DepartmentsDataSource.Department)
+           // {
+            //    Console.WriteLine("department xx  " + dept.Name + " " + dept.DepartmentID);
+                Console.WriteLine("department  " + dept.Name + " " + dept.DepartmentID);
+
+                IUsers usersSetDep = main._net2Client.ViewUserRecords("departmentId = '26'");
+
+                Console.WriteLine("hererere");
+
+                foreach (UsersSet.UserRow u in usersSetDep.UsersDataSource.User)
+                {
+                    //   Console.WriteLine("users in dep");
+
+            
+                }
+
+
+                //if (usersSetDep.UsersList().Count > 0)
+                //{
+                //
+                 //   Console.WriteLine("users in dep");
+                //}
+
+
+
+                        bool retdel = main._net2Client.DeleteDepartment(26);
+                Console.WriteLine("HERE " + retdel);
+
+                //if (retdel)
+                //{
+                //    Console.WriteLine(" department was delete  " + depString);
+                //}
+
+                retdel = main._net2Client.DeleteDepartment(23);
+                if (retdel)
+                {
+                    Console.WriteLine(" department was delete  " + depString);
+                }
+
+
+            }
+            */
+
+            // add new department
+
+            bool ret = main._net2Client.AddDepartment(depString);
+
+            if (ret)
+            {
+                Console.WriteLine("New department was added " + depString);
+            }
+
+            // get new department ID
+
+            IDepartments xxx = main._net2Client.ViewDepartments();
+
+            numID = 0;
+
+            foreach (DepartmentsSet.DepartmentRow dept in xxx.DepartmentsDataSource.Department)
+            {
+                //Console.WriteLine("here " + dept.Name + dept.DepartmentID);
+
+                if (dept.Name == depString)
+                {
+                    //Console.WriteLine("gotit" + dept.DepartmentID);
+                    numID = dept.DepartmentID;
+                    Console.WriteLine("department ID = " + numID + " for " + depString);
+
+                }
+
+            }
+
+            //Console.WriteLine("numID = " + numID);
+
+
+
+            int accessLevel = 1; // All hours, all doors
+            int departmentId = numID;
+            bool antiPassbackInd = false;
+            bool alarmUserInd = false;
+            string firstName = args[1];
+            string middleName = null;
+            string surname = args[2];
+            string telephoneNo = null;
+            string telephoneExtension = null;
+            string pinCode = args[6];
+            string pictureFileName = null;
+            //DateTime activationDate = DateTime.Now;
+            DateTime activationDate = DateTime.Parse(args[4]);
+
+            int cardNumber = 0;
+            int cardTypeID = 0;
+            bool active = true;
+            string faxNo = null;
+            DateTime expiryDate = DateTime.Parse(args[5]);
+            string[] customFields = null;
+
+            Console.WriteLine("New user about to be added " + args[1] + ' ' + args[2] + ' ' + args[6]);
+
+
+            int userId = main._net2Client.AddNewUser(
+                accessLevel, departmentId, antiPassbackInd, alarmUserInd, firstName, middleName, surname,
+                telephoneNo, telephoneExtension, pinCode, pictureFileName, activationDate, cardNumber, cardTypeID,
+                active, faxNo, expiryDate, customFields
+            );
+
+            if (userId == OemClient.ErrorCodes.AddNewUserFailed)
+            {
+                Console.Write(main._net2Client.LastErrorMessage);
+                main.Close();
+                return;
+            }
+
+            Console.WriteLine("New user was added " + args[1] + ' ' + args[6]);
+
+            // Get list of users in department
+
+            var query = "SELECT * from UsersEx";
+
+            var dataSet = main._net2Client.QueryDb(query);
+
+            foreach (DataRow row in dataSet.Tables[0].Rows)
+            {
+                Console.Write("row...");
+                Console.Write(row["Surname"]+"   ");
+                Console.WriteLine(row["DepartmentName"]);
+
+            }
+
+            main.Close();
+            return;
+        }
+
+
+        private bool AuthenticateUser(string userName, string password)
+        {
+            IOperators operators = _net2Client.GetListOfOperators();
+            Dictionary<int, string> operatorsList = operators.UsersDictionary();
+            foreach (int userID in operatorsList.Keys)
+            {
+                if (operatorsList[userID] == userName)
+                {
+                    Dictionary<string, int> methodList = _net2Client.AuthenticateUser(userID, password);
+                    return (methodList != null);
+                }
+            }
+            return false;
+        }
+
+        private void Close()
+        {
+            if (_net2Client != null)
+            {
+                _net2Client.Dispose();
+                _net2Client = null;
+            }
+        }
+
+       
+            
+            
+    }
+}
---
+++ get_latest.py
@@ -0,0 +1,84 @@
+"""
+A script to call api on visitor.express for latest
+"""
+import requests
+import datetime
+import json
+import sys
+import os
+from datetime import timedelta, date
+
+import subprocess
+
+pindir = os.path.normpath("C://Users/dave/ve/AddUser/bin/Debug/")
+
+os.chdir(pindir)
+
+dd = os.getcwd()
+
+logfile = open('logile', 'a')
+
+logfile.write('Start new run...\n')
+logfile.write(dd + 'dir...\n')
+
+host = "https://visitor.express/"
+#host = "http://localhost:8000/"
+#host = "https://visitor.express.netfm.org/"
+#host = "https://visitor.express.netfm.org/"
+token = "Token 0c7fb03fa775f0313364967ab0bb8255650cfd0c"
+carpark = "Visitors carpark (Car Park 2)"
+site = "Vodafone HQ"
+
+def get_latest(start, end):
+
+    start_str = start.strftime('%Y-%m-%d')
+    end_str = end.strftime('%Y-%m-%d')
+    dep_str = "Visitors_" + start.strftime('%m_%d')
+  
+
+    #print (start_str, end_str)
+    logfile.write(start_str + end_str + '...\n')
+
+
+    payload = {'carpark': carpark, 'site': site, 'start': start_str, 'end': end_str}
+    r = requests.post(host + "barrier/allowed_pins/", headers={"Authorization": token},
+                      data=payload)
+    assert r.status_code == 200
+#    print(r.text)
+    data = json.loads(r.text)
+
+
+    for record in data:
+        print(record)
+        print('\n')
+        user = record['first_name'] + ' ' + record['second_name'] + ' dummy_email@company.com ' + record['start'] + ' ' + record[
+'end'] + ' ' + str(format(record['pin_code'],"04"))
+        #print(format(record['pin_code'],"04"))  # To deal with zeros at front of 4 digit pins
+        cmd = 'AddUser.exe ' + ' ' + dep_str + ' ' + user
+        logfile.write(cmd + '\n')
+        subprocess.run(cmd)
+
+		
+if __name__ == "__main__":
+    start_date = date(2013, 1, 1)
+    end_date = date(2015, 6, 2)
+
+    start_date = datetime.datetime.now()
+
+    end_date = start_date + timedelta(days=3)
+    #start_date = start_date + timedelta(days=6)
+
+    
+    delta = end_date - start_date
+
+    for d in range(delta.days + 1):
+        start = start_date + timedelta(days=d)
+        end = start + timedelta(days=1)
+        #print( start, end)
+        get_latest(start, end)
+
+		
+
+		
+
+		
